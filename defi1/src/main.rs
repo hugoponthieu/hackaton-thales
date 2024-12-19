dcc6333c538f26d0b27b6cf4c28fcd201b48191e
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-12-19 10:33:38 +0100

    requete

--- defi1/src/main.rs
+++ defi1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+use serde_json::json;
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let res = client
+        .post("https://rust-checker.hackathon.dopolytech.fr/check")
+        .body(
+            json!({
+                "content": "fn main() { println!(\"Hello, world!\"); }",
+            })
+            .to_string(),
+        )
+        .send()
+        .await?;
+    let body = res.text().await?;
+    print!("{}", body);
+    Ok(())
 }
